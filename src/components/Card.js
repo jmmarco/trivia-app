4a4f57d073e3be548902aa9b6881cbf4f84db4c4
Juan Marco <jmmarco@gmail.com>
2020-02-21 12:03:42 -0300

    Simplify and organize component structure

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,40 @@
 import React from 'react';
+import { fetchQuestions } from '../utils/api'
 
-function Card() {
-  return (
-    <div className="card border">
-      <header className="card-header">
-        <h2>Entertainment: Video Games</h2>
-      </header>
-      <main className="card-main border">
-        <p>Unturned originally started as a Roblox game.</p>
-      </main>
-      <footer>
-        {1} of {10}
-      </footer>
-    </div>
-  );
+class Card extends React.Component {
+
+  state = {
+    questions: null
+  }
+
+  componentDidMount(){
+    fetchQuestions()
+      .then(questions => {
+        this.setState({
+          questions
+        })
+      })
+  }
+
+  render() {
+    const { questions } = this.state
+    return (
+      <div className="card border">
+        <header className="card-header">
+          <h2>Entertainment: Video Games</h2>
+        </header>
+        <main className="card-main border">
+          <p >Unturned originally started as a Roblox game.</p>
+    {questions && questions.map(q => <p key={q.question}>{q.question}</p>)}
+
+        </main>
+        <footer className="card-footer">
+          {1} of {10}
+        </footer>
+      </div>
+    );
+  }
+
 }
 
 export default Card;
