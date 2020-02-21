ecba55c7576d7b4e2978acfd9041401b7930fbc0
Juan Marco <jmmarco@gmail.com>
2020-02-21 19:17:00 -0300

    Use textarea hack to decode encoded question

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,22 @@
-export  function fetchQuestions() {
-  const endpoint = `https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean`
+export function fetchQuestions() {
+  const endpoint = encodeURI(
+    `https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean`
+  );
   return fetch(endpoint)
     .then(results => results.json())
     .then(data => {
       if (!data.results)
-        throw new Error('Unable to reach API, pleas try again later.')
-      
-      return data.results
-    })
+        throw new Error("Unable to reach API, pleas try again later.")
+      // Iterate over each question and remove encoded characters
+      const results = [
+        ...data.results.map(q => {
+          return { ...q, question: htmlDecode(q.question) }
+        })
+      ];
+      return results
+    });
 }
+
+// Decode encoded characters for each question
+const htmlDecode = innerHTML =>
+  Object.assign(document.createElement("textarea"), { innerHTML }).value
