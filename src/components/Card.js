9ac34614dc78aa585f0628700d8ca1fad7b3e23c
Juan Marco <jmmarco@gmail.com>
2020-02-21 19:18:16 -0300

    Add next and previous question functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,32 +4,55 @@ import { fetchQuestions } from '../utils/api'
 class Card extends React.Component {
 
   state = {
-    questions: null
+    questions: null,
+    index: 0
   }
 
   componentDidMount(){
     fetchQuestions()
       .then(questions => {
+        console.log(questions)
         this.setState({
           questions
         })
       })
   }
 
+  next = () => {
+    const { index, questions } = this.state
+    index < questions.length - 1 && (
+      this.setState((state) => {
+        return { index: state.index + 1}
+      })
+    )
+  }
+
+
+  prev = () => {
+    const { index, questions } = this.state
+    index > 0 && (
+      this.setState((state) => {
+        return { index: state.index - 1}
+      })
+    )
+  }
+
   render() {
-    const { questions } = this.state
+    const { index, questions } = this.state
     return (
       <div className="card border">
         <header className="card-header">
           <h2>Entertainment: Video Games</h2>
         </header>
         <main className="card-main border">
-          <p >Unturned originally started as a Roblox game.</p>
-    {questions && questions.map(q => <p key={q.question}>{q.question}</p>)}
+          {/* <p >Unturned originally started as a Roblox game.</p> */}
+          {questions && <p>{questions[index].question}</p>}
+        <button onClick={this.prev} disabled={index === 0}>Prev</button>
+        <button onClick={this.next} disabled={questions && index === questions.length -1}>Next</button>
 
         </main>
         <footer className="card-footer">
-          {1} of {10}
+          {index + 1} of {questions && questions.length}
         </footer>
       </div>
     );
