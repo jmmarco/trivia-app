8be4f2a72f2526fe70f2b2dd4ad50a2470830610
Juan Marco <jmmarco@gmail.com>
2020-02-24 13:41:59 -0300

    Separate body into functional component

---
+++ src/components/CardBody.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+  FiXCircle,
+  FiCheckCircle,
+  FiThumbsUp,
+  FiThumbsDown
+} from "react-icons/fi";
+
+function CardBody({ questions, index, checkAnswer }) {
+  return (
+    <>
+      <header className="card-header">
+        <h2>{questions && questions[index].category}</h2>
+
+        {questions[index].result === "correct" && (
+          <FiThumbsUp size={40} color="limegreen" />
+        )}
+        {questions[index].result === "incorrect" && (
+          <FiThumbsDown size={40} color="crimson" />
+        )}
+      </header>
+      <main className="card-main border">
+        {questions && (
+          <p className="text-center">{questions[index].question}</p>
+        )}
+
+        <nav className="card-nav">
+          <button
+            className={`btn-nav ${questions[index].result && "disabled"}`}
+            disabled={questions[index].result}
+            onClick={() => checkAnswer(true)}
+          >
+            <FiCheckCircle size={60} />
+          </button>
+          <button
+            className={`btn-nav ${questions[index].result && "disabled"}`}
+            disabled={questions[index].result}
+            onClick={() => checkAnswer(false)}
+          >
+            <FiXCircle size={60} />
+          </button>
+        </nav>
+      </main>
+      <footer className="card-footer">
+        {index + 1} of {questions && questions.length}
+      </footer>
+    </>
+  );
+}
+
+export default CardBody
