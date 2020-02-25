f3ee85cef118e7720d89b4aafceba94a6dd69551
Juan Marco <jmmarco@gmail.com>
2020-02-25 11:13:34 -0300

    Add + and - using CSS and some color

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+function whatever() {
+  console.log('whatever')
+}
+
 function Results({ questions, handleReset }) {
   const score = questions.reduce((acc, q) => {
     if (q.result === "correct") {
@@ -8,17 +12,42 @@ function Results({ questions, handleReset }) {
     return acc;
   }, 0);
 
+
+  const handleClick = event => {
+    event.target.classList.toggle('hide-some-text')
+    event.target.classList.toggle('show-some-text')
+  }
+
+  const scorePercentage = (score / questions.length) * 100;
   return (
     <>
-      <p>
+      <h3>
         You answered {score} of {questions.length} questions!
-      </p>
-      {score / questions.length <= 0.5 && <p>You need practice!</p>}
-      {score / questions.length > 0.5 && score / questions.length <= 0.8 && (
-        <p>Pretty good!</p>
+      </h3>
+      {score / questions.length <= 0.5 && (
+        <p>That's {scorePercentage}%. You need practice!</p>
       )}
-      {score / questions.length > 0.8 && <p>Excellent!</p>}
-      <button className="btn-reset" onClick={handleReset}>
+      {score / questions.length > 0.5 && score / questions.length <= 0.8 && (
+        <p>That's {scorePercentage}%. Pretty good!</p>
+      )}
+      {score / questions.length > 0.8 && (
+        <p>That's {scorePercentage}%. Excellent!</p>
+      )}
+      <h4 className="review-heading">Review</h4>
+      <div className="results-questions">
+        {questions.map(q => {
+          return (
+            <p
+              key={q.question}
+              className={`results-question hide-some-text ${q.result}`}
+              onClick={handleClick}
+            >
+              {q.question} {q.correct_answer}
+            </p>
+          );
+        })}
+      </div>
+      <button className="btn-standard" onClick={handleReset}>
         Play Again!
       </button>
     </>
