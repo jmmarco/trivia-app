d578b2026bc99f4f01442e85a1145b8297888b72
Juan Marco <jmmarco@gmail.com>
2020-02-25 20:49:54 -0300

    Add getQuestions action and reducer

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,6 +1,6 @@
 import { GET_QUESTIONS } from '../actions/questions'
 
-export default function questions(state = [], action) {
+export default function questionsReducer(state = [], action) {
   switch(action.type) {
     case GET_QUESTIONS:
       return [
