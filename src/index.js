176402a17e4f4a7bca8771f671fc2fe9fc72bd68
Juan Marco <jmmarco@gmail.com>
2020-02-25 19:41:35 -0300

    Create redux store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './components/Card';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import questions from './reducers/questions'
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+const store = createStore(questions)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+console.log(store.getState())
+
+ReactDOM.render(<Provider store={store}><Card /></Provider>, document.getElementById('root'));
