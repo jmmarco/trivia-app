f999c3559488df44b424540bdeada7e312d85cb0
Juan Marco <jmmarco@gmail.com>
2020-02-26 20:41:20 -0300

    Add actions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,8 @@
 export const GET_QUESTIONS = 'GET_QUESTIONS';
+export const LOAD_QUESTIONS = 'LOAD_QUESTIONS';
+export const ERROR = 'ERROR';
+export const NEXT_QUESTION = 'NEXT_QUESTION';
+export const ANSWER_QUESTION = 'ANSWER_QUESTION';
 
 export function getQuestions(questions) {
   return {
@@ -6,3 +10,33 @@ export function getQuestions(questions) {
     questions
   }
 }
+
+export function loadQuestions(boolean) {
+  return {
+    type: LOAD_QUESTIONS,
+    isLoading: boolean
+  }
+}
+
+export function loadError(error) {
+  return {
+    type: ERROR,
+    message: error
+  }
+}
+
+
+export function answerQuestion(boolean) {
+  return {
+    type: ANSWER_QUESTION,
+    answer: boolean
+  }
+}
+
+export function nextQuestion(index) {
+  return  {
+    type: NEXT_QUESTION,
+    index
+  }
+}
+
--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,11 +1,20 @@
 import { fetchQuestions } from '../utils/api'
-import { getQuestions } from './questions'
+import { getQuestions, loadQuestions, loadError } from './questions'
+
 
 export function handleInitialData() {
   return (dispatch) => {
     return fetchQuestions()
       .then(questions => {
+        dispatch(loadQuestions(true))
         dispatch(getQuestions(questions))
+        dispatch(loadQuestions(false))
+      })
+      .catch(error => {
+        dispatch(loadQuestions(false))
+        dispatch(loadError(error))
       })
   }
 }
+
+
