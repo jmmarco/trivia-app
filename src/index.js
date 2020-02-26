2310af34c3649ae4d1b5d6225a07eae5e768cbf1
Juan Marco <jmmarco@gmail.com>
2020-02-26 20:42:22 -0300

    Pass store to app

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 import Card from "./components/Card";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import questionsReducer from "./reducers/questions";
+import reducer from "./reducers";
 import middleware from "./middleware";
 
-const store = createStore(questionsReducer, middleware);
+const store = createStore(reducer, middleware);
 
-console.log(store.getState());  
+// console.log(store.getState());  
 
 ReactDOM.render(
   <Provider store={store}>
