516dbaf7295432ae6f9e13d5769443594ad6dc9e
Juan Marco <jmmarco@gmail.com>
2020-02-27 19:41:32 -0300

    No need to combine reducers

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import  { questions, loading, error, index } from './questions'
-
-export default combineReducers({
-  questions,
-  loading,
-  error,
-  index
-})
