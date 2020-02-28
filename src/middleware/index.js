d29a24559c5bc3fe2434e1d9f3f4512e4731972a
Juan Marco <jmmarco@gmail.com>
2020-02-28 14:06:06 -0300

    Add comments

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,7 @@
-import logger from './logger'
-import thunk from 'redux-thunk'
-import { applyMiddleware } from 'redux'
+// import logger from './logger' // Uncomment for extra debugging
+import thunk from "redux-thunk";
+import { applyMiddleware } from "redux";
 
-export default applyMiddleware(thunk, logger)
+export default applyMiddleware(thunk);
+// Comment the line above and uncomment the line below for extra debugging info
+// export default applyMiddleware(thunk, logger)
