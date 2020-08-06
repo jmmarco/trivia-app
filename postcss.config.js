d72832511a79584a3f9eabfb7d90af9e2feda313
Juan Marco <jmmarco@gmail.com>
2020-08-06 17:41:56 -0300

    Setup postCSS for production

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  // defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
 
 module.exports = {
